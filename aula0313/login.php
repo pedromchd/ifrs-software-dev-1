bb8f4bac6ac1fb82590494f6ab3fc0f84e4eac22
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-03-13 11:30:48 -0300

    Aula 13/03

--- aula0313/login.php
+++ aula0313/login.php
@@ -10,12 +10,12 @@
 </head>
 
 <body class="h-screen grid place-items-center">
-  <main class="">
-    <form action="authenticate.php" method="post">
-      <label for="">Username: <input type="text" name="username" id=""></label>
-      <label for="">Password: <input type="password" name="password" id=""></label>
-      <input type="submit" value="Login">
-      <a href="register.php"><input type="button" value="Register"></a>
+  <main class="p-4 border border-black">
+    <form class="space-y-2" action="authenticate.php" method="post">
+      <label class="block" for="">Username:<br><input class="p-1 border-[1px] border-black focus:outline-none" type="text" name="username" id=""></label>
+      <label class="block" for="">Password:<br><input class="p-1 border-[1px] border-black focus:outline-none" type="password" name="password" id=""></label>
+      <input class="py-1 px-2 border border-black cursor-pointer" type="submit" value="Login">
+      <a href="register.php"><input class="py-1 px-2 border border-black cursor-pointer" type="button" value="Register"></a>
     </form>
   </main>
 </body>
--- aula0313/register.php
+++ aula0313/register.php
@@ -6,17 +6,20 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Register</title>
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-  <form action="insert_data.php" method="post">
-    <label for="">Email: <input type="text" name="email" id=""></label>
-    <label for="">Full name: <input type="text" name="fullname" id=""></label>
-    <label for="">Username: <input type="text" name="username" id=""></label>
-    <label for="">Password: <input type="password" name="password" id=""></label>
-    <input type="submit" value="Register">
-    <a href="login.php"><input type="button" value="Back"></a>
-  </form>
+<body class="h-screen grid place-items-center">
+  <main class="p-4 border border-black">
+    <form class="space-y-2" action="insert_data.php" method="post">
+      <label class="block" for="">Email:<br><input class="p-1 border-[1px] border-black focus:outline-none" type="text" name="email" id=""></label>
+      <label class="block" for="">Full name:<br><input class="p-1 border-[1px] border-black focus:outline-none" type="text" name="fullname" id=""></label>
+      <label class="block" for="">Username:<br><input class="p-1 border-[1px] border-black focus:outline-none" type="text" name="username" id=""></label>
+      <label class="block" for="">Password:<br><input class="p-1 border-[1px] border-black focus:outline-none" type="password" name="password" id=""></label>
+      <input class="py-1 px-2 border border-black cursor-pointer" type="submit" value="Register">
+      <a href="login.php"><input class="py-1 px-2 border border-black cursor-pointer" type="button" value="Back"></a>
+    </form>
+  </main>
 </body>
 
 </html>
