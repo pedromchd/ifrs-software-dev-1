8960817073d38604b68b9b5f107ade66ced102ad
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-04-10 11:41:22 -0300

    Aula 10/04

--- aula0410/app/Http/Controllers/MyController.php
+++ aula0410/app/Http/Controllers/MyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\game;
 use Illuminate\Http\Request;
 
 class MyController extends Controller
@@ -11,8 +12,33 @@ class MyController extends Controller
     return view('welcome');
   }
 
-  public function geraMyHome(Request $request) {
+  public function getData()
+  {
+    $games = game::all();
+    echo "
+    <table>
+      <tr>
+        <td>Name</td>
+        <td>Year</td>
+        <td>System</td>
+        <td>Developer</td>
+      </tr>
+    ";
+    foreach ($games as $game) : ?>
+      <tr>
+        <td><?= $game->name ?></td>
+        <td><?= $game->year ?></td>
+        <td><?= $game->system ?></td>
+        <td><?= $game->developer ?></td>
+      </tr>
+    <?php endforeach;
+    echo "</table>";
+  }
+
+  public function geraMyHome(Request $request)
+  {
     $data = $request->all();
+    $this->getData();
     return view('myHomeView', ['mydata' => $data['email'], 'mypass' => $data['password']]);
   }
 }
---
+++ aula0410/app/Models/game.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class game extends Model
+{
+  use HasFactory;
+
+  protected $fillable = ['name', 'year', 'system', 'developer'];
+
+  protected $table = 'game';
+
+  public $timestamps = false;
+}
