898acf361f903f4c7b729da5fd531428c0e12584
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-07-02 13:03:24 -0300

    remove

--- aula0626/controllers/contentController.js
+++
@@ -1,7 +0,0 @@
-exports.home = (req, res) => {
-  res.send('Página inicial');
-};
-
-exports.about = (req, res) => {
-  res.send('Sobre nós');
-};
--- aula0626/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const contentController = require('./controllers/contentController');
-
-router.get('/', contentController.home);
-router.get('/about', contentController.about);
-
-module.exports = router;
