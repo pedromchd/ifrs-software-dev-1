d61005ba05f687c0db536b3bd189881b1acaec45
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-07-02 11:41:55 -0300

    remove

--- aula0626/app/controllers/HomeController.js
+++
@@ -1,9 +0,0 @@
-// Exemplo de HomeController.js
-
-exports.index = (req, res) => {
-  res.render('index', { title: 'Página inicial' });
-};
-
-exports.about = (req, res) => {
-  res.render('about', { title: 'Sobre' });
-};
