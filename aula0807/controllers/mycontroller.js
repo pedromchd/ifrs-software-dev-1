904012d532fe4b3303c67ceb5322083aad738295
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-08-07 11:24:00 +0000

    Aula 07/08

---
+++ aula0807/controllers/mycontroller.js
@@ -0,0 +1,45 @@
+const { sequelize, Sequelize } = require('../config/database')
+
+const livrosModel = require('../models/livros')(sequelize, Sequelize)
+
+exports.showForm = (req, res) => {
+    res.render("myform", { layout: false });
+}
+
+exports.save = (req, res) => {
+    const bookSetData = {
+        title: req.body.title,
+        description: req.body.description
+    };
+
+    livrosModel.create(bookSetData).then(data => {
+        console.log('Data saved');
+        res.redirect('/')
+    }).catch(err => {
+        console.log("Error" + err)
+    })
+
+}
+
+exports.showResult = (req, res) => {
+
+    livrosModel.findAll(
+        {
+            order: [['title', 'ASC']]
+
+        }).then(results => {
+            //console.log('results'+results.json)
+
+            res.render("myresult", { resultado: results });
+
+        }).catch(err => {
+            console.log("Error" + err)
+            res.status(500).send({ message: "Error" + err.message })
+        })
+
+
+
+
+
+}
+
